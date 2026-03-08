6b3c369d7df96ccdf3c7390227cc69edd67cc5df
safalsood <safal2k1@gmail.com>
2026-03-08 15:51:18 +0530

    Fix env loading for Render

--- loadEnv.js
+++ loadEnv.js
@@ -1,7 +1,13 @@
-import fs from 'fs'
-      
-      const envConfig = JSON.parse(fs.readFileSync('env.json', 'utf8'));
-      
-      Object.keys(envConfig).forEach(key => {
-        process.env[key] = envConfig[key];
-      });
+import fs from "fs";
+
+try {
+  if (fs.existsSync("env.json")) {
+    const env = JSON.parse(fs.readFileSync("env.json", "utf8"));
+    Object.assign(process.env, env);
+    console.log("Loaded environment variables from env.json");
+  } else {
+    console.log("env.json not found, using environment variables from Render");
+  }
+} catch (error) {
+  console.error("Error loading env.json:", error);
+}
